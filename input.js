ad63ac61d6ff22b5af874bf38a69e11ec9b8eee1
Jose jorge <prueba@gmail.com>
2023-03-09 21:53:30 -0400

    challenger done!

--- input.js
+++ input.js
@@ -1,26 +1,18 @@
 //VARIABLE
 const email = document.querySelector('#email');
 const form = document.querySelector('#form');
+const alertImage = document.querySelector('#error-icon');
+const error = document.querySelector('.error-email');
 
 
 
 form.addEventListener('input', (e) =>{
     e.preventDefault();
     if (!email.value.match(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/)) {
-        showAlert()
-        return false;
-    } else{
-        showAlert(error.textContent = '')
-        return;
         
+        return false;
     }
-
+    error.remove();
+    alertImage.remove();
+    return true
 })
-function showAlert() {
-    const error = document.createElement('P');
-        email.style.border = "solid 3px";
-        email.style.borderColor = "#fa6161";
-        error.textContent = 'Please provid a valid email';
-        error.classList.add('error-email');
-        form.appendChild(error);
-}
